b8096645168a82dacde5013decb952f6d3f622ed
itkyujin <104597854+itkyujin@users.noreply.github.com>
2022-04-30 10:24:58 -0400

    Add files via upload

---
+++ commit099.java
@@ -0,0 +1,35 @@
+package commit.comp1011.commit099;
+
+import java.util.Stack;
+
+
+class Commit099 {
+
+    public int commit(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(board[j][move - 1]);
+                        board[j][move - 1] = 0;
+                        break;
+                    }
+                    if (board[j][move - 1] == stack.peek()) {
+                        stack.pop();
+                        answer += 2;
+                    } else
+                        stack.push(board[j][move - 1]);
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
